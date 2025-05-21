7d191cbd225812d8b82f35e1790e0c9a3000f881
rishitha6274 <rishithalattupally01@gmail.com>
2025-05-21 20:55:38 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import Product from "./components/Product";
 import Cart from "./components/Cart";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
 function App() {
   return (
-    <>
-     <div>
-       <BrowserRouter>
-         <Router>
-           <header>
-            <h1>React Store</h1>
-            <link to="/">Home</link>
-            <link to="/cart">Cart</link>
-            <hr />
-           </header>
-           <main>
-            <Routes>
-              <Route index element={<Product/>}/>
-              <Route path="/" element={<Product/>}></Route>
-              <Route path="/cart" element={<Cart/>}></Route>
-            </Routes>
-           </main>
-           <footer>
-            <hr />
-            &copy; 2025 All rights Reserved.
-           </footer>
-         </Router>
-       </BrowserRouter>
-     </div>
-    </>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <header>
+          <h1>React Store</h1>
+          <Link to="/">Home</Link>-
+          <Link to="/cart">Cart</Link>
+          <hr />
+        </header>
 
-export default App
+        <main>
+          <Routes>
+            <Route index element={<Product />} />
+            <Route path="/" element={<Product/>}></Route>
+            <Route path="/cart" element={<Cart/>}></Route>
+          </Routes>
+        </main>
+
+        <footer>
+          <hr />
+          &copy; 2005. All rights Reserved.
+        </footer>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
