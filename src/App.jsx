5b9226693371c96017a39e1ba216eaba117c6e3e
rishitha6274 <rishithalattupally01@gmail.com>
2025-05-21 20:53:18 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
      <div>
-       <header>
-        <h1>React Store</h1>
-        <hr />
-       </header>
-       <main>
-        <h2>Products List</h2>
-       </main>
-       <footer>
-        <hr />
-        &copy; 2025 All rights Reserved.
-       </footer>
+       <BrowserRouter>
+         <Router>
+           <header>
+            <h1>React Store</h1>
+            <link to="/">Home</link>
+            <link to="/cart">Cart</link>
+            <hr />
+           </header>
+           <main>
+            <Routes>
+              <Route index element={<Product/>}/>
+              <Route path="/" element={<Product/>}></Route>
+              <Route path="/cart" element={<Cart/>}></Route>
+            </Routes>
+           </main>
+           <footer>
+            <hr />
+            &copy; 2025 All rights Reserved.
+           </footer>
+         </Router>
+       </BrowserRouter>
      </div>
     </>
   )
