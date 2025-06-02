ea79c0e11de96376eaba773ac7cef7c8af151aad
rishitha6274 <rishithalattupally01@gmail.com>
2025-06-02 14:28:58 +0530

    updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -26,7 +26,7 @@ export default function Product() {
   return (
     <div className="form-container">
       {user && <h2 className="form-title">Welcome, {user.name}!</h2>}
-      <p style={{ color: "#d86c7a" }}>Product List</p>
+      <p style={{ color: "#d86c7a" }}>Product List</p><br />
 
       <ul style={{ listStyle: "none", padding: 0 }}>
         {products.map(product => (
